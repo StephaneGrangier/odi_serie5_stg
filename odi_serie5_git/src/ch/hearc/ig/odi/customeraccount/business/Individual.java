666d516dbfca3644203e0fdce29779906ac4e697
Floriane Sapin <floriane.sapin@he-arc.ch>
2016-03-10 21:13:53 +0100

    ajout de la classe individual

---
+++ odi_serie5_git/src/ch/hearc/ig/odi/customeraccount/business/Individual.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hearc.ig.odi.customeraccount.business;
+
+import java.util.Date;
+
+/**
+ *
+ * @author floriane.sapin
+ */
+public class Individual extends Customer{
+    
+    private Date birthDate;
+    private String email;
+    
+    public Individual(Integer number, String firstname, String lastName, Date birthDate, String email){
+        super(number, firstname, lastName);
+        this.birthDate = birthDate;
+        this.email = email;   
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
